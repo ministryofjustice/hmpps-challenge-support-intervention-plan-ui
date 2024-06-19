79be2d846d34384159d80ebdd076b7432c0295bf
bill-moj <167539377+bill-moj@users.noreply.github.com>
2024-06-19 16:10:13 +0100

    Fix ts strict error (#25)

--- server/middleware/insertJourneyIdentifier.ts
+++ server/middleware/insertJourneyIdentifier.ts
@@ -3,7 +3,7 @@ import { v4 as uuidV4, validate } from 'uuid'
 
 export default function insertJourneyIdentifier() {
   return (req: Request, res: Response, next: NextFunction): void => {
-    const uuid = req.url.split('/')[1]
+    const uuid = req.url.split('/')[1] || '/'
     if (!validate(uuid)) {
       return res.redirect(`${req.baseUrl}/${uuidV4()}${req.url}`)
     }
