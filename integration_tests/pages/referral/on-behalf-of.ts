24a02ca2d621a57d24513b1fbeb645c0adfaf9f5
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-06-24 14:14:08 +0100

    Add comment

--- integration_tests/pages/referral/on-behalf-of.ts
+++ integration_tests/pages/referral/on-behalf-of.ts
@@ -5,5 +5,6 @@ export default class OnBehalfOfPage extends Page {
     super('Referrals (On Behalf Of) asdadasdwq is under construction...')
   }
 
+  // TODO: replace this with assertion of data-qa=mini-profile being rendered (Left here for now to provide working example of cy.findByText)
   miniProfileDob = (): PageElement => cy.findByText('Date of birth').next('span')
 }
