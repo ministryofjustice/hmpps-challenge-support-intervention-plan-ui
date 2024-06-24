372730c4bd4b7e5c59725c83b1519dbdb0175d1d
James Reed <james.reed@digital.justice.gov.uk>
2024-06-24 15:10:11 +0100

    chore: same-origin referrer

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -16,7 +16,7 @@ export default function setUpWebSecurity(): Router {
   })
   router.use(
     helmet({
-      referrerPolicy: { policy: 'origin' },
+      referrerPolicy: { policy: 'same-origin' },
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
