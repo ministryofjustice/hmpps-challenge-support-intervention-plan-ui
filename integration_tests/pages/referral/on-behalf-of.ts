8b3e4cefa1e46dd6b95d5ce89e3f73e88f961d3d
James Reed <james.reed@digital.justice.gov.uk>
2024-06-27 17:02:05 +0100

    chore: fix cy test

--- integration_tests/pages/referral/on-behalf-of.ts
+++ integration_tests/pages/referral/on-behalf-of.ts
@@ -2,7 +2,7 @@ import Page, { PageElement } from '../page'
 
 export default class OnBehalfOfPage extends Page {
   constructor() {
-    super('Referrals (On Behalf Of) asdadasdwq is under construction...')
+    super(`Are you making this referral on someone else's behalf?`)
   }
 
   miniProfileHyperlink = (name: string): PageElement => cy.findByRole('link', { name: new RegExp(name, 'i') })
