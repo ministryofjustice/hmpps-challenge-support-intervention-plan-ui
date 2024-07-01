d63f3fef0fc6d0ed0ec0ac75d1c84b205a7c830d
James Reed <james.reed@digital.justice.gov.uk>
2024-07-01 16:31:52 +0100

    chore: add new test, fix content, make routes closer to where they happen

--- server/journeys/referral/routes.ts
+++ server/journeys/referral/routes.ts
@@ -2,13 +2,13 @@ import { RequestHandler, Router } from 'express'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import { ReferralRootController } from './controller'
 import { OnBehalfOfRoutes } from './on-behalf-of/routes'
-import type CsipApiService from '../../services/csipApi/csipApiService'
+import CsipApiService from '../../services/csipApi/csipApiService'
 import { ReferralAreaOfWorkRoutes } from './area-of-work/routes'
-import type PrisonerSearchService from '../../services/prisonerSearch/prisonerSearchService'
+import PrisonerSearchService from '../../services/prisonerSearch/prisonerSearchService'
 import StartJourneyRoutes from './start/routes'
 
 function Routes(csipApiService: CsipApiService): Router {
-  const router = Router()
+  const router = Router({ mergeParams: true })
 
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
@@ -25,9 +25,8 @@ function Routes(csipApiService: CsipApiService): Router {
 export default function routes(csipApiService: CsipApiService, prisonerSearchService: PrisonerSearchService): Router {
   const router = Router({ mergeParams: true })
 
-  router.use('/referral', Routes(csipApiService))
-
   router.use('/', StartJourneyRoutes(prisonerSearchService))
+  router.use('/referral', Routes(csipApiService))
 
   return router
 }
--- server/journeys/referral/start/tests.test.ts
+++ server/journeys/referral/start/tests.test.ts
@@ -4,25 +4,42 @@ import request from 'supertest'
 import { appWithAllRoutes } from '../../../routes/testutils/appSetup'
 import type PrisonerSearchService from '../../../services/prisonerSearch/prisonerSearchService'
 import type Prisoner from '../../../services/prisonerSearch/prisoner'
+import { SanitisedError } from '../../../sanitisedError'
+import type CsipApiService from '../../../services/csipApi/csipApiService'
 
 let app: Express
 const uuid = uuidv4()
+const csipApiService = {
+  getReferenceData: () => [
+    { code: 'A', description: 'TEXT' },
+    { code: 'B', description: 'TEXT2' },
+  ],
+} as unknown as CsipApiService
 const prisonerSearchService = {
   getPrisonerDetails: async () => {
     return {
       cellLocation: '',
       firstName: 'Bob',
       lastName: 'Marley',
-      prisonerNumber: 'ABC123'
+      prisonerNumber: 'ABC123',
     } as Prisoner
-  }
+  },
+} as unknown as PrisonerSearchService
+
+const prisonerSearchService404 = {
+  getPrisonerDetails: async () => {
+    const err = new Error('404') as SanitisedError
+    err.status = 404
+    throw err
+  },
 } as unknown as PrisonerSearchService
 beforeEach(() => {
   app = appWithAllRoutes({
     services: {
+      csipApiService,
       prisonerSearchService,
     },
-    uuid
+    uuid,
   })
 })
 
@@ -37,6 +54,29 @@ describe('tests', () => {
       .expect(302)
       .redirects(1)
       .expect('Location', /referral\/on-behalf-of/)
+      .end(err => {
+        if (err) {
+          done(err)
+          return
+        }
+        done()
+      })
+  })
+
+  it('should redirect to dps prisoner search page if prisoner not found', done => {
+    request(
+      appWithAllRoutes({
+        services: {
+          csipApiService,
+          prisonerSearchService: prisonerSearchService404,
+        },
+        uuid,
+      }),
+    )
+      .get(`/prisoners/ABC123/referral/start`)
+      .expect(302)
+      .redirects(1)
+      .expect('Location', /localhost:3001/)
       .end(err => {
         if (err) {
           done(err)
