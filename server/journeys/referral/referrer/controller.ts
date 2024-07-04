852a62ebe994f0556206ae01ff19d0f2b72fe93f
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-07-04 16:15:01 +0100

    Fix error input handling

--- server/journeys/referral/referrer/controller.ts
+++ server/journeys/referral/referrer/controller.ts
@@ -3,11 +3,12 @@ import { BaseJourneyController } from '../../base/controller'
 
 export class ReferralReferrerController extends BaseJourneyController {
   GET = async (req: Request, res: Response): Promise<void> => {
+    console.log(res.locals['formResponses'])
     const areaOfWorkOptions = await this.getReferenceDataOptions(
       req,
       'area-of-work',
       'Select area',
-      req.journeyData.referral!.refererArea,
+      res.locals['formResponses']?.['areaOfWork'] || req.journeyData.referral!.refererArea,
     )
     const { referredBy } = req.journeyData.referral!
     res.render('referral/referrer/view', { areaOfWorkOptions, referredBy, backUrl: 'on-behalf-of' })
