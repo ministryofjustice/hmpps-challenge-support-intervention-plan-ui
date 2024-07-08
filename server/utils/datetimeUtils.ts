d572e7e4b47db876dc16f3de941ef94e082d8749
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-07-08 15:55:50 +0100

    Address PR comments

--- server/utils/datetimeUtils.ts
+++ server/utils/datetimeUtils.ts
@@ -1,23 +1,19 @@
+import z from 'zod'
+
 const DATE_FORMAT_GB = new Intl.DateTimeFormat('en-GB', {
   year: 'numeric',
   month: '2-digit',
   day: '2-digit',
 })
 
-class ParseResult {
-  constructor(public data?: string) {}
+const RESULT_VALIDATOR = z.string().min(1)
 
-  get success() {
-    return !!this.data
-  }
-}
-
-const parseNumber = (value: string, min: number, max: number, length: number): ParseResult => {
-  if (Number.isNaN(value) || Number(value) < min || Number(value) > max || value.length > length) {
-    return new ParseResult()
-  }
-
-  return new ParseResult(value.padStart(length, '0'))
+const parseNumber = (value: string, min: number, max: number, length: number) => {
+  const result =
+    Number.isNaN(value) || Number(value) < min || Number(value) > max || value.length > length
+      ? null
+      : value.padStart(length, '0')
+  return RESULT_VALIDATOR.safeParse(result)
 }
 
 export const parse24Hour = (value: string) => parseNumber(value, 0, 23, 2)
@@ -25,15 +21,13 @@ export const parse24Hour = (value: string) => parseNumber(value, 0, 23, 2)
 export const parseMinute = (value: string) => parseNumber(value, 0, 59, 2)
 
 // parse dd/mm/yyyy date string from Datepicker component into yyyy-mm-dd format
-export const parseDate = (value: string): ParseResult => {
-  if (value.length !== 10) {
-    return new ParseResult()
-  }
-  const date = new Date(Date.parse(`${value.substring(6, 10)}-${value.substring(3, 5)}-${value.substring(0, 2)}`))
-  if (Number.isNaN(date)) {
-    return new ParseResult()
-  }
-  return new ParseResult(date.toISOString().substring(0, 10))
+export const parseDate = (value: string) => {
+  const date =
+    value.length !== 10
+      ? null
+      : new Date(Date.parse(`${value.substring(6, 10)}-${value.substring(3, 5)}-${value.substring(0, 2)}`))
+  const result = date === null || Number.isNaN(date) ? null : date.toISOString().substring(0, 10)
+  return RESULT_VALIDATOR.safeParse(result)
 }
 
 // format ISO Date into GB date string
