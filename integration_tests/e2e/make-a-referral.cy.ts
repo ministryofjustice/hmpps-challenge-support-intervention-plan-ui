3b3935420453f5b5ec715bb7a1eaa47f0158ebd0
James Reed <james.reed@digital.justice.gov.uk>
2024-07-17 10:57:32 +0100

    feat: e2e test

--- integration_tests/e2e/make-a-referral.cy.ts
+++ integration_tests/e2e/make-a-referral.cy.ts
@@ -3,6 +3,11 @@ context('Make a Referral Journey', () => {
     cy.task('reset')
     cy.task('stubSignIn')
     cy.task('stubGetPrisoner')
+    cy.task('stubAreaOfWork')
+    cy.task('stubIncidentLocation')
+    cy.task('stubIncidentType')
+    cy.task('stubIncidentInvolvement')
+    cy.task('stubContribFactors')
   })
 
   it('happy path', () => {
@@ -12,8 +17,7 @@ context('Make a Referral Journey', () => {
     cy.findByRole('radio', { name: /no/i }).click()
     cy.findByRole('button', { name: /continue/i }).click()
 
-    cy.findByRole('combobox').select(1)
-    cy.findByRole('combobox').invoke('attr', 'val').as('areaOfWork', { type: 'static' })
+    cy.findByRole('combobox').select('AreaA')
     cy.findByRole('button', { name: /continue/i }).click()
 
     cy.findByRole('radio', { name: /reactive/i }).click()
@@ -21,42 +25,44 @@ context('Make a Referral Journey', () => {
 
     cy.findByRole('heading', { name: /incident details/i }).should('be.visible')
     cy.findByRole('heading', { name: /when did the incident occur\?/i }).should('be.visible')
-    cy.findByRole('input', { name: /date of incident/i }).type('06/07/2024')
-    cy.findByRole('input', { name: /hour/i }).type('12')
-    cy.findByRole('input', { name: /minute/i }).type('24')
-    cy.findByRole('combobox', { name: /where did the incident occur\?/i }).select(1)
-    cy.findByRole('combobox', { name: /where did the incident occur\?/i })
-      .invoke('attr', 'val')
-      .as('incidentLocation', { type: 'static' })
-    cy.findByRole('combobox', { name: /what was the incident type\?/i }).select(1)
-    cy.findByRole('combobox', { name: /what was the incident type\?/i })
-      .invoke('attr', 'val')
-      .as('incidentType', { type: 'static' })
+    cy.findByRole('textbox', { name: /date of incident/i }).type('06/07/2024')
+    cy.findByRole('textbox', { name: /hour/i }).type('12')
+    cy.findByRole('textbox', { name: /minute/i }).type('24')
+    cy.findByRole('combobox', { name: /where did the incident occur\?/i }).select('LocationB')
+    cy.findByRole('combobox', { name: /what was the incident type\?/i }).select('TypeA')
     cy.findByRole('button', { name: /continue/i }).click()
 
     cy.findByRole('heading', { name: /incident involvement/i }).should('be.visible')
-    cy.findByRole('radio', { name: /other/i }).click()
+    cy.findByRole('radio', { name: /factor1/i }).click()
     cy.findByRole('radio', { name: /yes/i }).click()
     cy.findByRole('textbox').type('TestStaff Member')
     cy.findByRole('button', { name: /continue/i }).click()
 
     cy.findByRole('heading', { name: /describe the incident and concerns/i }).should('be.visible')
-    cy.findByText(/the description could include/i).should('not.be.visible')
+    cy.findByText(/the description could include/i).then(el => {
+      expect(el.get(0).checkVisibility()).to.eq(false)
+    })
     cy.findByText(/what type of information to include/i).click()
-    cy.findByText(/the description could include/i).should('be.visible')
+    cy.findByText(/the description could include/i).then(el => {
+      expect(el.get(0).checkVisibility()).to.eq(true)
+    })
     cy.findByRole('textbox').type('incident concerns foobar123')
     cy.findByRole('button', { name: /continue/i }).click()
 
     cy.findByRole('heading', { name: /what reasons have been given for the incident\?/i }).should('be.visible')
-    cy.findByText(/include any reasons given/i).should('not.be.visible')
+    cy.findByText(/include any reasons given/i).then(el => {
+      expect(el.get(0).checkVisibility()).to.eq(false)
+    })
     cy.findByText(/what type of information to include/i).click()
-    cy.findByText(/include any reasons given/i).should('not.be.visible')
+    cy.findByText(/include any reasons given/i).then(el => {
+      expect(el.get(0).checkVisibility()).to.eq(true)
+    })
     cy.findByRole('textbox').type('incident reasons foobar123')
     cy.findByRole('button', { name: /continue/i }).click()
 
     cy.findByRole('heading', { name: /what are the contributory factors\?/i }).should('be.visible')
-    cy.findByRole('checkbox', { name: /bullying/i }).click()
-    cy.findByRole('checkbox', { name: /medication/i }).click()
+    cy.findByRole('checkbox', { name: /factor1/i }).click()
+    cy.findByRole('checkbox', { name: /factor3/i }).click()
     cy.findByRole('button', { name: /continue/i }).click()
 
     cy.findAllByRole('link', { name: /add comment/i }).should('have.length', 2)
@@ -64,7 +70,8 @@ context('Make a Referral Journey', () => {
       .first()
       .should('be.visible')
       .click()
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByRole('heading', { name: /add a comment on factor1 factors \(optional\)\?/i }).should('be.visible')
+    cy.findByRole('link', { name: /^back/i }).click()
     cy.findAllByRole('link', { name: /add comment/i })
       .first()
       .should('be.visible')
@@ -80,7 +87,7 @@ context('Make a Referral Journey', () => {
     cy.findByRole('radio', { name: /yes/i }).click()
     cy.findByRole('button', { name: /continue/i }).click()
 
-    cy.findByRole('heading', { name: /add additional information \(optional\)\?/i }).should('be.visible')
+    cy.findByRole('heading', { name: /add additional information \(optional\)/i }).should('be.visible')
     cy.findByRole('textbox').type('additional info')
     cy.findByRole('button', { name: /continue/i }).click()
 
@@ -90,51 +97,83 @@ context('Make a Referral Journey', () => {
     cy.go('back')
 
     cy.findByDisplayValue('additional info').should('be.visible')
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByRole('link', { name: /^back/i }).click()
 
     cy.findByRole('radio', { name: /yes/i }).should('be.checked')
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByRole('link', { name: /^back/i }).click()
 
     cy.findByRole('link', { name: /edit comment/i }).should('be.visible')
     cy.findByRole('link', { name: /edit comment/i }).click()
     cy.findByDisplayValue('factor comment').should('be.visible')
-    cy.findByRole('link', { name: /back/i }).click()
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByRole('link', { name: /^back/i }).click()
+    cy.findByRole('link', { name: /^back/i }).click()
 
-    cy.findByRole('checkbox', { name: /bullying/i }).should('be.checked')
-    cy.findByRole('checkbox', { name: /medication/i }).should('be.checked')
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByRole('checkbox', { name: /factor1/i }).should('be.checked')
+    cy.findByRole('checkbox', { name: /factor3/i }).should('be.checked')
+    cy.findByRole('link', { name: /^back/i }).click()
 
     cy.findByDisplayValue('incident reasons foobar123').should('be.visible')
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByRole('link', { name: /^back/i }).click()
 
     cy.findByDisplayValue('incident concerns foobar123').should('be.visible')
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByRole('link', { name: /^back/i }).click()
 
     cy.findByDisplayValue('TestStaff Member').should('be.visible')
     cy.findByRole('radio', { name: /yes/i }).should('be.checked')
-    cy.findByRole('radio', { name: /other/i }).should('be.checked')
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByRole('radio', { name: /factor1/i }).should('be.checked')
+    cy.findByRole('link', { name: /^back/i }).click()
 
     cy.findByDisplayValue('06/07/2024').should('be.visible')
     cy.findByDisplayValue('12').should('be.visible')
     cy.findByDisplayValue('24').should('be.visible')
-    cy.get('@incidentLocation').then(val => {
-      cy.findByDisplayValue(val as unknown as string).should('be.visible')
-    })
-    cy.get('@incidentType').then(val => {
-      cy.findByDisplayValue(val as unknown as string).should('be.visible')
-    })
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByDisplayValue('LocationB').should('be.visible')
+    cy.findByDisplayValue('TypeA').should('be.visible')
+    cy.findByRole('link', { name: /^back/i }).click()
 
     cy.findByRole('radio', { name: /reactive/i }).should('be.checked')
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByRole('link', { name: /^back/i }).click()
 
-    cy.get('@areaOfWork').then(val => {
-      cy.findByDisplayValue(val as unknown as string).should('be.visible')
-    })
-    cy.findByRole('link', { name: /back/i }).click()
+    cy.findByDisplayValue('AreaA').should('be.visible')
+    cy.findByRole('link', { name: /^back/i }).click()
 
     cy.findByRole('radio', { name: /no/i }).should('be.checked')
+
+    cy.findByRole('radio', { name: /yes/i }).click()
+    cy.findByRole('button', { name: /continue/i }).click()
+
+    cy.findByDisplayValue(/john smith/i).should('be.visible')
+    cy.findByRole('button', { name: /continue/i }).click()
+
+    cy.findByRole('radio', { name: /proactive/i }).click()
+    cy.findByRole('button', { name: /continue/i }).click()
+
+    cy.findByRole('heading', { name: /behaviour details/i }).should('be.visible')
+    cy.findByDisplayValue('06/07/2024').should('be.visible')
+    cy.findByDisplayValue('12').should('be.visible')
+    cy.findByDisplayValue('24').should('be.visible')
+    cy.findByDisplayValue('LocationB').should('be.visible')
+    cy.findByDisplayValue('TypeA').should('be.visible')
+    cy.findByRole('button', { name: /continue/i }).click()
+
+    cy.findByRole('heading', { name: /behaviour involvement/i }).should('be.visible')
+    cy.findByDisplayValue('TestStaff Member').should('be.visible')
+    cy.findByRole('radio', { name: /yes/i }).should('be.checked')
+    cy.findByRole('radio', { name: /factor1/i }).should('be.checked')
+    cy.findByRole('button', { name: /continue/i }).click()
+
+    cy.findByRole('heading', { name: /describe the behaviour and concerns/i }).should('be.visible')
+    cy.findByDisplayValue('incident concerns foobar123').should('be.visible')
+    cy.findByRole('button', { name: /continue/i }).click()
+
+    cy.findByRole('heading', { name: /what reasons have been given for the behaviour\?/i }).should('be.visible')
+    cy.findByDisplayValue('incident reasons foobar123').should('be.visible')
+    cy.findByRole('button', { name: /continue/i }).click()
+
+    cy.findByRole('button', { name: /continue/i }).click()
+    cy.findByRole('button', { name: /continue/i }).click()
+    cy.findByRole('button', { name: /continue/i }).click()
+    cy.findByRole('button', { name: /continue/i }).click()
+
+    // TODO: We should be back at CYA page, which isn't implemented yet - recheck changed answers now
   })
 })
