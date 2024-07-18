5c1b54c81f2aaa2fa5daadb87185c0a99021943d
James Reed <james.reed@digital.justice.gov.uk>
2024-07-18 13:33:05 +0100

    chore: breadcrumbs

--- server/journeys/referral/confirmation/controller.ts
+++ server/journeys/referral/confirmation/controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 export class ConfirmationController {
   GET = async (_req: Request, res: Response): Promise<void> => {
     res.render('referral/confirmation/view', {
-      backUrl: false,
+      showBreadcrumbs: true,
     })
   }
 }
