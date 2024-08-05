8db00b67540cb84ae1e902aa3537c3d90947f2f4
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2024-08-05 16:33:15 +0100

    patch for some spelling errors on wrong test names

--- server/journeys/record-investigation/evidence-secured/controller.ts
+++ server/journeys/record-investigation/evidence-secured/controller.ts
@@ -2,15 +2,16 @@ import { Request, Response } from 'express'
 import { SchemaType } from './schemas'
 
 export class EvidenceSecuredController {
-  GET = async (req: Request, res: Response): Promise<void> => {
+  GET = async (req: Request, res: Response) => {
+    const evidenceSecured =
+      res.locals.formResponses?.['evidenceSecured'] || req.journeyData.investigation?.evidenceSecured
     res.render('record-investigation/evidence-secured/view', {
-      evidenceSecured:
-        res.locals?.formResponses?.['evidenceSecured'] || req.journeyData?.investigation?.evidenceSecured,
+      evidenceSecured,
       backUrl: '../record-investigation',
     })
   }
 
-  POST = async (req: Request<unknown, unknown, SchemaType>, res: Response): Promise<void> => {
+  POST = async (req: Request<unknown, unknown, SchemaType>, res: Response) => {
     req.journeyData.investigation!.evidenceSecured = req.body.evidenceSecured
     res.redirect('../record-investigation')
   }
