ae79d3da34293bfbe7f9a2bc3059a0643aa42b1f
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2024-08-07 13:54:37 +0100

    chore: replace hmpps date picker with moj one (#105)
    * chore: replace hmpps date picker with moj one
    
    * chore: remove unneeded block

--- server/journeys/referral/details/schemas.ts
+++ server/journeys/referral/details/schemas.ts
@@ -1,7 +1,7 @@
 import z from 'zod'
 import { Request } from 'express'
 import CsipApiService from '../../../services/csipApi/csipApiService'
-import { parse24Hour, parseDate, parseMinute, todayString } from '../../../utils/datetimeUtils'
+import { parse24Hour, parseMinute } from '../../../utils/datetimeUtils'
 
 export const schemaFactory =
   (csipApiService: CsipApiService) =>
@@ -22,6 +22,10 @@ export const schemaFactory =
       ? 'Date of the most recent occurrence must be today or in the past'
       : 'Date of the incident must be today or in the past'
 
+    const INCIDENT_DATE_INVALID_MSG = req.journeyData.referral!.isProactiveReferral
+      ? 'Date of the most recent occurrence must be a real date'
+      : 'Date of the incident must be a real date'
+
     const incidentLocationMap = new Map(
       (await csipApiService.getReferenceData(req, 'incident-location')).map(itm => [itm.code, itm]),
     )
@@ -33,15 +37,28 @@ export const schemaFactory =
       .object({
         incidentDate: z
           .string({ message: INCIDENT_DATE_MSG })
-          .superRefine((val, ctx) => {
-            const result = parseDate(val)
-            if (!result.success) {
-              ctx.addIssue({ code: z.ZodIssueCode.custom, message: INCIDENT_DATE_MSG })
-            } else if (result.data! > todayString()) {
-              ctx.addIssue({ code: z.ZodIssueCode.custom, message: INCIDENT_DATE_FUTURE_MSG })
-            }
+          .min(8, { message: INCIDENT_DATE_MSG })
+          .max(10, { message: INCIDENT_DATE_MSG })
+          .transform(value => value.split(/[-/]/).reverse())
+          .transform(value => {
+            // Prefix month and date with a 0 if needed
+            const month = value[1]?.length === 2 ? value[1] : `0${value[1]}`
+            const date = value[2]?.length === 2 ? value[2] : `0${value[2]}`
+            return `${value[0]}-${month}-${date}T00:00:00Z` // We put a full timestamp on it so it gets parsed as UTC time and the date doesn't get changed due to locale
           })
-          .transform(val => parseDate(val).data),
+          .pipe(
+            z.coerce
+              .date({
+                errorMap: (issue, ctx) => {
+                  if (issue.code === 'invalid_date') {
+                    return { message: INCIDENT_DATE_INVALID_MSG }
+                  }
+                  return { message: ctx.defaultError }
+                },
+              })
+              .max(new Date(), { message: INCIDENT_DATE_FUTURE_MSG }),
+          )
+          .transform(dateString => dateString.toISOString().substring(0, 10)),
         hour: z.string().transform(val => (val?.length ? parse24Hour(val) : null)),
         minute: z.string().transform(val => (val?.length ? parseMinute(val) : null)),
         incidentLocation: z
