cb7dff716bc6bf9869705a7484bfcf512aed4fff
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-08-08 18:21:28 +0100

    update test

--- server/journeys/record-investigation/interviews-summary/tests.cy.ts
+++ server/journeys/record-investigation/interviews-summary/tests.cy.ts
@@ -1,4 +1,6 @@
 context('test /record-investigation/interviews-summary', () => {
+  const uuid = 'a232c47e-4096-4bf5-8ee5-3d6397136d01'
+
   beforeEach(() => {
     cy.task('reset')
     cy.task('stubSignIn')
@@ -17,12 +19,15 @@ context('test /record-investigation/interviews-summary', () => {
     proceedToAddInterview()
     cy.go('back')
 
+    assertPageWithSubmittedInterview()
+    assertInterviewCommentFallbackDisplay()
+
     continueToTaskList()
   })
 
   const navigateToTestPage = () => {
     cy.signIn()
-    cy.visit('/csip-record/02e5854f-f7b1-4c56-bec8-69e390eb8550/record-investigation/start')
+    cy.visit(`${uuid}/csip-record/02e5854f-f7b1-4c56-bec8-69e390eb8550/record-investigation/start`)
     cy.url().should('to.match', /\/record-investigation$/)
     cy.findByRole('link', { name: /Interview details/i }).click()
   }
@@ -32,8 +37,82 @@ context('test /record-investigation/interviews-summary', () => {
     cy.url().should('to.match', /\/interview-details$/)
   }
 
+  const assertPageWithSubmittedInterview = () => {
+    injectJourneyData({
+      investigation: {
+        interviews: [
+          {
+            interviewee: 'Some Person',
+            interviewDate: '2024-12-25',
+            intervieweeRole: { code: 'CODE', description: 'Witness' },
+            interviewText: 'some text',
+          },
+        ],
+      },
+    })
+
+    cy.findByRole('heading', { name: /Interviews summary/ }).should('be.visible')
+    cy.findByRole('heading', { name: /Interview with Some Person/ }).should('be.visible')
+    cy.findByRole('link', { name: /delete interview with some person/i })
+      .should('be.visible')
+      .click()
+    cy.url().should('to.match', /delete-interview\/1$/)
+    cy.go('back')
+
+    cy.contains('dt', 'Interviewee').next().should('include.text', 'Some Person')
+    cy.findByRole('link', { name: /change the interviewee's name for interview 1/i })
+      .should('be.visible')
+      .click()
+    cy.url().should('to.match', /interview-details\/1#interviewee$/)
+    cy.go('back')
+
+    cy.contains('dt', 'Interview date').next().should('include.text', '25 December 2024')
+    cy.findByRole('link', { name: /change the interview date for interview 1/i })
+      .should('be.visible')
+      .click()
+    cy.url().should('to.match', /interview-details\/1#interviewDate$/)
+    cy.go('back')
+
+    cy.contains('dt', 'Role').next().should('include.text', 'Witness')
+    cy.findByRole('link', { name: /change the interviewee's role for interview 1/i })
+      .should('be.visible')
+      .click()
+    cy.url().should('to.match', /interview-details\/1#intervieweeRole$/)
+    cy.go('back')
+
+    cy.contains('dt', 'Comments').next().should('include.text', 'some text')
+    cy.findByRole('link', { name: /change the comments for interview 1/i })
+      .should('be.visible')
+      .click()
+    cy.url().should('to.match', /interview-details\/1#interviewText$/)
+    cy.go('back')
+  }
+
+  const assertInterviewCommentFallbackDisplay = () => {
+    injectJourneyData({
+      investigation: {
+        interviews: [
+          {
+            interviewee: 'Some Person',
+            interviewDate: '2024-12-25',
+            intervieweeRole: { code: 'CODE', description: 'Witness' },
+            interviewText: undefined,
+          },
+        ],
+      },
+    })
+
+    cy.contains('dt', 'Comments').next().should('include.text', 'Not provided')
+  }
+
   const continueToTaskList = () => {
     cy.findByRole('button', { name: /Continue/i }).click()
     cy.url().should('to.match', /\/record-investigation$/)
+  }
+
+  const injectJourneyData = (json: object) => {
+    const data = btoa(JSON.stringify(json))
+    cy.request('GET', `/${uuid}/inject-journey-data?data=${data}`)
+    cy.reload()
   }
 })
--- server/routes/journeyRoutes.ts
+++ server/routes/journeyRoutes.ts
@@ -4,8 +4,9 @@ import referralRoutes from '../journeys/referral/routes'
 import screenRoutes from '../journeys/screen/routes'
 import investigationRoutes from '../journeys/record-investigation/routes'
 import populatePrisonerSummary from '../middleware/populatePrisonerSummary'
+import config from '../config'
 
-export default function journeyRoutes(services: Services): Router {
+export default function journeyRoutes(services: Services) {
   const router = Router({ mergeParams: true })
 
   router.use(populatePrisonerSummary())
@@ -13,6 +14,18 @@ export default function journeyRoutes(services: Services): Router {
   router.use('/', referralRoutes({ services, path: '/referral' }))
   router.use('/', screenRoutes({ services, path: '/screen' }))
   router.use('/', investigationRoutes({ services, path: '/record-investigation' }))
+
+  if (config.environmentName === 'dev') {
+    router.get('/inject-journey-data', (req, res) => {
+      const { data } = req.query
+      const json = JSON.parse(atob(data as string))
+      Object.entries(json).forEach(([key, value]) => {
+        // @ts-expect-error unexpected types
+        req.journeyData[key] = value
+      })
+      res.sendStatus(200)
+    })
+  }
 
   return router
 }
