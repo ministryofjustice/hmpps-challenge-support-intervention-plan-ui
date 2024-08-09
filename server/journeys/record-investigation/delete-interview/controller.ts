04504841ac1b2c0227fff7a4f67c33de3b4367ac
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-08-09 17:19:05 +0100

    handle invalid interview index

--- server/journeys/record-investigation/delete-interview/controller.ts
+++ server/journeys/record-investigation/delete-interview/controller.ts
@@ -3,10 +3,18 @@ import { Request, Response } from 'express'
 export class DeleteInterviewController {
   GET = async (req: Request, res: Response) => {
     const { index } = req.params
-    res.render('record-investigation/delete-interview/view', {
-      interview: req.journeyData.investigation!.interviews![Number(index) - 1],
-      backUrl: '../interviews-summary',
-    })
+
+    if (Number.isNaN(index) || (req.journeyData.investigation!.interviews?.length || 0) < Number(index)) {
+      res.render('pages/error', {
+        message: 'Interview not found',
+        status: 404,
+      })
+    } else {
+      res.render('record-investigation/delete-interview/view', {
+        interview: req.journeyData.investigation!.interviews![Number(index) - 1],
+        backUrl: '../interviews-summary',
+      })
+    }
   }
 
   POST = async (req: Request, res: Response) => {
