12b9bb34ebffda16c49eb09d4e0ec495b71aaea1
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2024-08-12 16:50:58 +0100

    fix pipeline?

--- server/journeys/record-investigation/interviews-summary/controller.ts
+++ server/journeys/record-investigation/interviews-summary/controller.ts
@@ -4,7 +4,7 @@ export class InterviewsSummaryController {
   GET = async (req: Request, res: Response) => {
     res.render('record-investigation/interviews-summary/view', {
       interviews: req.journeyData.investigation!.interviews || [],
-      newInterviewIndex: req.journeyData.investigation!.interviews ? req.journeyData.investigation!.interviews.length + 1 : 1,
+      newInterviewIndex: (req.journeyData.investigation!.interviews || []).length + 1,
     })
   }
 
