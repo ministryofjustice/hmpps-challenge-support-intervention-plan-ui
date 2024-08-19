18b068217bfaec0a95a804db6ea452ebd7189930
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2024-08-19 15:12:10 +0100

    chore: remove extra tsconfig, fix a couple imports (#137)
    * chore: remove extra tsconfig, fix a couple imports
    
    * chore: remove unused tsconfig

--- integration_tests/e2e/health.cy.ts
+++ integration_tests/e2e/health.cy.ts
@@ -1,3 +1,5 @@
+import { expect } from 'chai'
+
 context('Healthcheck', () => {
   context('All healthy', () => {
     beforeEach(() => {
--- integration_tests/e2e/make-a-referral.cy.ts
+++ integration_tests/e2e/make-a-referral.cy.ts
@@ -1,3 +1,4 @@
+import { expect } from 'chai'
 import { checkAxeAccessibility } from '../support/accessibilityViolations'
 
 context('Make a Referral Journey', () => {
