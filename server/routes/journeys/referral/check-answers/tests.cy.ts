1acfd0ab32712f110a845d87dfc072ab8147e926
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2024-08-22 16:03:48 +0100

    PR comments

--- server/routes/journeys/referral/check-answers/tests.cy.ts
+++ server/routes/journeys/referral/check-answers/tests.cy.ts
@@ -77,12 +77,49 @@ context('test /record-investigation/check-answers', () => {
 
     cy.findByRole('heading', { name: /Check your answers before sending the referral/ }).should('be.visible')
 
+    checkBehalfOf()
+    checkNameOfReferrer()
+    checkAreaOfWork()
+    checkProactiveReactive()
+
+    checkProactiveReactiveChange()
+
+    cy.findByRole('link', { name: /Change if the referral is proactive or reactive/i })
+      .should('be.visible')
+      .click()
+
+    cy.findByRole('radio', { name: /Reactive/i }).click()
+    getContinueButton().click()
+
+    checkDateOfIncident()
+    checkTimeOfIncident()
+    checkLocationOfIncident()
+    checkIncidentType()
+    checkIncidentInvolvement()
+    checkStaffAssaulted()
+    checkDescriptionOfConcern()
+    checkReasonsGiven()
+    checkContributoryFactors()
+    checkCommentOnFactor2()
+    checkSaferCustody()
+    checkAdditionalInformation()
+
+    continueToConfirmation()
+  })
+
+  const checkBehalfOf = () => {
     cy.contains('dt', 'Referral made on behalf of someone else').next().should('include.text', `No`)
     cy.findByRole('link', { name: /Change if the referral is being made on behalf of someone else or not/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /on-behalf-of/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+
+    cy.findByRole('link', { name: /Change if the referral is being made on behalf of someone else or not/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('radio', { name: /Yes/i }).should('be.focused').click()
     getContinueButton().click()
@@ -90,33 +127,60 @@ context('test /record-investigation/check-answers', () => {
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Referral made on behalf of someone else').next().should('include.text', `Yes`)
     cy.contains('dt', 'Name of referrer').next().should('include.text', `Test Person`)
+  }
+
+  const checkNameOfReferrer = () => {
     cy.findByRole('link', { name: /Change name of referrer/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /referrer/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+
+    cy.findByRole('link', { name: /Change name of referrer/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('textbox').should('be.focused').clear().type(ESCAPE_LITTLE, { delay: 0 })
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Name of referrer').next().should('include.text', `<script>alert("Test User")</script>`)
+  }
+
+  const checkAreaOfWork = () => {
     cy.contains('dt', 'Area of work').next().should('include.text', `AreaA`)
     cy.findByRole('link', { name: /Change area of work/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /referrer/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+
+    cy.findByRole('link', { name: /Change area of work/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('combobox', { name: 'Which area do they work in?' }).should('be.focused').select('AreaB')
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Area of work').next().should('include.text', `AreaB`)
+  }
+
+  const checkProactiveReactive = () => {
     cy.contains('dt', 'Proactive or reactive referral').next().should('include.text', `Reactive`)
     cy.findByRole('link', { name: /Change if the referral is proactive or reactive/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /proactive-or-reactive/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+
+    cy.findByRole('link', { name: /Change if the referral is proactive or reactive/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('radio', { name: /Proactive/i })
       .should('be.focused')
@@ -124,80 +188,116 @@ context('test /record-investigation/check-answers', () => {
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Proactive or reactive referral').next().should('include.text', `Proactive`)
+  }
 
-    checkProactiveReactiveChange()
-
-    // Go back and keep on Reactive
-    cy.findByRole('link', { name: /Change if the referral is proactive or reactive/i })
-      .should('be.visible')
-      .click()
-
-    cy.findByRole('radio', { name: /Reactive/i }).click()
-    getContinueButton().click()
-    //
-
+  const checkDateOfIncident = () => {
     cy.contains('dt', 'Date of incident').next().should('include.text', `22 August 2024`)
     cy.findByRole('link', { name: /Change date of incident/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /details/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change date of incident/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('textbox', { name: 'Date of incident' }).should('be.focused').clear().type('21/08/2024', { delay: 0 })
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Date of incident').next().should('include.text', `21 August 2024`)
+  }
+
+  const checkTimeOfIncident = () => {
     cy.contains('dt', 'Time of incident').next().should('include.text', `01:01`)
     cy.findByRole('link', { name: /Change time of incident/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /details/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change time of incident/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('textbox', { name: 'Hour' }).should('be.focused').clear().type('2', { delay: 0 })
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Time of incident').next().should('include.text', `2`)
+  }
+
+  const checkLocationOfIncident = () => {
     cy.contains('dt', 'Location of incident').next().should('include.text', `LocationA`)
     cy.findByRole('link', { name: /Change location of incident/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /details/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change location of incident/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('combobox', { name: 'Where did the incident occur?' }).should('be.focused').select('LocationB')
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Location of incident').next().should('include.text', `LocationB`)
+  }
+
+  const checkIncidentType = () => {
     cy.contains('dt', 'Incident type').next().should('include.text', `TypeA`)
     cy.findByRole('link', { name: /Change incident type/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /details/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change incident type/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('combobox', { name: 'What was the incident type?' }).should('be.focused').select('TypeB')
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Incident type').next().should('include.text', `TypeB`)
+  }
+
+  const checkIncidentInvolvement = () => {
     cy.contains('dt', 'Prisoner involvement').next().should('include.text', `TypeA`)
     cy.findByRole('link', { name: /Change how the prisoner was involved/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /involvement/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change how the prisoner was involved/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('radio', { name: /Factor1/i }).should('be.focused')
     cy.findByRole('radio', { name: /Factor2/i }).click()
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Prisoner involvement').next().should('include.text', `Factor2`)
+  }
+
+  const checkStaffAssaulted = () => {
     cy.contains('dt', 'Staff assaulted').next().should('include.text', `No`)
     cy.findByRole('link', { name: /Change if a staff member was assaulted or not/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /involvement/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change if a staff member was assaulted or not/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('radio', { name: /No/i })
     cy.findByRole('radio', { name: /Yes/i }).should('be.focused').click()
@@ -208,78 +308,116 @@ context('test /record-investigation/check-answers', () => {
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Staff assaulted').next().should('include.text', `Yes`)
     cy.contains('dt', 'Names of staff assaulted').next().should('include.text', `<script>alert("Test User")</script>`)
-    cy.findByRole('link', { name: /Change the name of the staff members assaulted/i })
-      .should('be.visible')
-      .click()
+  }
 
-    cy.url().should('to.match', /involvement/)
-
-    cy.findByRole('textbox').should('be.focused').clear().type('Not Jimmy', { delay: 0 })
-    getContinueButton().click()
-    cy.url().should('to.match', /check-answers/)
-    cy.contains('dt', 'Names of staff assaulted').next().should('include.text', `Not Jimmy`)
+  const checkDescriptionOfConcern = () => {
     cy.contains('dt', 'Description of incident and concerns').next().should('include.text', `idklol`)
     cy.findByRole('link', { name: /Change the description of the incident and concerns/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /description/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change the description of the incident and concerns/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('textbox').should('be.focused').clear().type(ESCAPE_AND_FORMAT, { delay: 0 })
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Description of incident and concerns').next().should('include.text', ESCAPE_AND_FORMAT_EXPECTED)
+  }
+
+  const checkReasonsGiven = () => {
     cy.contains('dt', 'Reasons given for the incident').next().should('include.text', `idklol`)
     cy.findByRole('link', { name: /Change the reasons given for the incident/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /reasons/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change the reasons given for the incident/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('textbox').should('be.focused').clear().type(ESCAPE_AND_FORMAT, { delay: 0 })
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Reasons given for the incident').next().should('include.text', ESCAPE_AND_FORMAT_EXPECTED)
+  }
+
+  const checkContributoryFactors = () => {
     cy.contains('dt', 'Contributory factors').next().should('include.text', `Factor1`)
     cy.findByRole('link', { name: /Change the contributory factors/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /contributory-factors/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change the contributory factors/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('checkbox', { name: /Factor1/i }).should('be.focused')
     cy.findByRole('checkbox', { name: /Factor2/i }).click()
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Contributory factors').next().should('include.text', `Factor2`)
+  }
+
+  const checkCommentOnFactor2 = () => {
     cy.contains('dt', 'Comment on factor2 factors').next().should('include.text', `Not provided`)
     cy.findByRole('link', { name: /Change the comment on factor2 factors/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /code2-comment/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change the comment on factor2 factors/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('textbox').should('be.focused').clear().type(ESCAPE_AND_FORMAT, { delay: 0 })
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Comment on factor2 factors').next().should('include.text', ESCAPE_AND_FORMAT_EXPECTED)
+  }
+
+  const checkSaferCustody = () => {
     cy.contains('dt', 'Safer Custody aware of referral').next().should('include.text', `I don’t know`)
     cy.findByRole('link', { name: /Change if Safer Custody are aware of the referral or not/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /safer-custody/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change if Safer Custody are aware of the referral or not/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('radio', { name: /Yes/i }).should('be.focused').click()
     getContinueButton().click()
     cy.url().should('to.match', /check-answers/)
     cy.contains('dt', 'Safer Custody aware of referral').next().should('include.text', `Yes`)
+  }
+
+  const checkAdditionalInformation = () => {
     cy.contains('dt', 'Other information relating to this referral').next().should('include.text', `Not provided`)
     cy.findByRole('link', { name: /Change the additional information relating to the referral/i })
       .should('be.visible')
       .click()
 
     cy.url().should('to.match', /additional-information/)
+    cy.go('back')
+    cy.url().should('to.match', /check-answers/)
+    cy.findByRole('link', { name: /Change the additional information relating to the referral/i })
+      .should('be.visible')
+      .click()
 
     cy.findByRole('textbox').should('be.focused').clear().type(ESCAPE_AND_FORMAT, { delay: 0 })
     getContinueButton().click()
@@ -287,8 +425,7 @@ context('test /record-investigation/check-answers', () => {
     cy.contains('dt', 'Other information relating to this referral')
       .next()
       .should('include.text', ESCAPE_AND_FORMAT_EXPECTED)
-    continueToConfirmation()
-  })
+  }
 
   const checkProactiveReactiveChange = () => {
     cy.contains('dt', 'Date of occurrence').next().should('include.text', `22 August 2024`)
