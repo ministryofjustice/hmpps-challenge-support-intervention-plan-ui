e878f9fc5c5ab88c64848355788ec9525f78687e
JackReeveMoJ <jack.reeve@digital.justice.gov.uk>
2024-08-27 09:13:30 +0100

    remove custom UI ordering (#158)

--- server/routes/journeys/screen/controller.ts
+++ server/routes/journeys/screen/controller.ts
@@ -4,12 +4,10 @@ import { BaseJourneyController } from '../base/controller'
 
 export class ScreenController extends BaseJourneyController {
   GET = async (req: Request, res: Response) => {
-    const outcomeTypeItems = this.customOrderRadios(
-      await this.getReferenceDataOptionsForRadios(
-        req,
-        'screening-outcome-type',
-        res.locals.formResponses?.['outcomeType'] || req.journeyData.saferCustodyScreening?.outcomeType,
-      ),
+    const outcomeTypeItems = await this.getReferenceDataOptionsForRadios(
+      req,
+      'screening-outcome-type',
+      res.locals.formResponses?.['outcomeType'] || req.journeyData.saferCustodyScreening?.outcomeType,
     )
 
     res.render('screen/view', {
@@ -23,16 +21,5 @@ export class ScreenController extends BaseJourneyController {
     req.journeyData.saferCustodyScreening!.outcomeType = req.body.outcomeType
     req.journeyData.saferCustodyScreening!.reasonForDecision = req.body.reasonForDecision
     res.redirect('screen/check-answers')
-  }
-
-  /** Moves 'No further action' to the bottom of the list */
-  private customOrderRadios = (items: { value: string; text: string | undefined }[]) => {
-    const nfaIndex = items.findIndex(o => o.value === 'NFA')
-    if (nfaIndex > -1) {
-      const nfa = items[nfaIndex]!
-      items.splice(nfaIndex, 1)
-      items.push(nfa)
-    }
-    return items
   }
 }
