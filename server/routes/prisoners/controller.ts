5ed3228778cad0a60f511911f4a066fdb1c4def5
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-08-29 11:14:48 +0100

    Simplify pagination

--- server/routes/prisoners/controller.ts
+++ server/routes/prisoners/controller.ts
@@ -14,40 +14,18 @@ export class PrisonersController {
     const { prisonNumber } = req.params
     const { page } = req.query
 
-    const pageNumber = Number.isNaN(Number(page)) ? 1 : Number(page)
+    const currentPage = Number.isNaN(Number(page)) ? 1 : Number(page)
 
     const prisoner = await this.prisonerSearchService.getPrisonerDetails(req, prisonNumber!)
-    const csipSummaries = await this.csipApiService.getPrisonerCsipRecords(req, prisonNumber!, pageNumber, PAGE_SIZE)
+    const csipSummaries = await this.csipApiService.getPrisonerCsipRecords(req, prisonNumber!, currentPage, PAGE_SIZE)
 
-    const pageCount = Math.ceil(csipSummaries.metadata.totalElements / PAGE_SIZE)
+    const totalPages = Math.ceil(csipSummaries.metadata.totalElements / PAGE_SIZE)
 
     res.render('prisoners/view', {
       prisoner,
       csipRecords: csipSummaries.content,
-      pagination:
-        pageCount > 1
-          ? {
-              previous:
-                pageNumber > 1
-                  ? {
-                      href: `?page=${pageNumber - 1}`,
-                    }
-                  : undefined,
-              next:
-                pageCount > pageNumber
-                  ? {
-                      href: `?page=${pageNumber + 1}`,
-                    }
-                  : undefined,
-              items: [...Array(pageCount).keys()]
-                .map(i => i + 1)
-                .map(index => ({
-                  number: index,
-                  current: index === pageNumber,
-                  href: `?page=${index}`,
-                })),
-            }
-          : undefined,
+      totalPages,
+      currentPage,
       showBreadcrumbs: true,
     })
   }
