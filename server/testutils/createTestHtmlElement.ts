8dd765b7fc0aa3a6fd16fcfe5267d9292ac13245
James Reed <james.reed@digital.justice.gov.uk>
2024-09-19 14:46:56 +0100

    chore: first pass, ensure stats are same as main

--- server/testutils/createTestHtmlElement.ts
+++
@@ -1,7 +0,0 @@
-export default function createTestHtmlElement(text: string) {
-  const div = document.createElement('div')
-  div.innerHTML = text
-  document.body.innerHTML = ''
-  document.body.appendChild(div)
-  return document.documentElement
-}
