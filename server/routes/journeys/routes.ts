8368e3b7d18d41ec780ef0e1125f8519cb93c051
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2024-09-26 13:11:57 +0100

    added tests

--- server/routes/journeys/routes.ts
+++ server/routes/journeys/routes.ts
@@ -25,16 +25,14 @@ export const JourneyRoutes = (services: Services) => {
   router.use('/', UpdateInvestigationRoutes({ services, path: '/update-investigation' }))
 
   /* eslint-disable no-param-reassign */
-  const mergeObjects = <T>(destination: T, source: Partial<T>) => {
+  const mergeObjects = <T extends Record<string, unknown>>(destination: T, source: Partial<T>) => {
     Object.entries(source).forEach(([key, value]) => {
       if (typeof value === 'object' && !Array.isArray(value)) {
-        // @ts-expect-error unexpected types
         if (!destination[key]) {
           // @ts-expect-error set up object for future recursive writes
           destination[key] = {}
         }
-        // @ts-expect-error unexpected types
-        mergeObjects(destination[key], value)
+        mergeObjects(destination[key] as Record<string, unknown>, value)
       } else {
         // @ts-expect-error unexpected types
         destination[key] = value
