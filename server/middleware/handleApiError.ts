4d721aaf95c8b185b8285874c8214dafafaddcc8
bill-moj <bill.yeung@digital.justice.gov.uk>
2024-10-09 13:41:38 +0100

    fix tests

--- server/middleware/handleApiError.ts
+++ server/middleware/handleApiError.ts
@@ -18,6 +18,7 @@ export const handleApiError = (error: HTTPError, req: Request, res: Response, ne
     } else {
       next(error)
     }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (_e) {
     next(error)
   }
