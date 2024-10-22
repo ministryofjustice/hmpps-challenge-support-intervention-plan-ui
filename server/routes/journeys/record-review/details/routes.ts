4bee2f3fecdb6a2dc24a3b5298a2d4d2c8d4e9fa
JackReeveMoJ <jack.reeve@digital.justice.gov.uk>
2024-10-22 17:31:15 +0100

    MIC-686: UaR: Enter review details (#285)

--- server/routes/journeys/record-review/details/routes.ts
+++ server/routes/journeys/record-review/details/routes.ts
@@ -1,6 +1,6 @@
 import { SummaryController } from './controller'
 import { validate } from '../../../../middleware/validationMiddleware'
-import { schemaFactory } from './schemas'
+import { schema } from './schemas'
 import { JourneyRouter } from '../../base/routes'
 
 export const SummaryRoutes = () => {
@@ -8,7 +8,7 @@ export const SummaryRoutes = () => {
   const controller = new SummaryController()
 
   get('/', controller.GET)
-  post('/', validate(schemaFactory), controller.POST)
+  post('/', validate(schema), controller.POST)
 
   return router
 }
--- server/routes/journeys/record-review/details/schemas.ts
+++ server/routes/journeys/record-review/details/schemas.ts
@@ -1,24 +1,14 @@
 import z from 'zod'
-import { Request, Response } from 'express'
 import { createSchema } from '../../../../middleware/validationMiddleware'
-import { getMaxCharsAndThresholdForAppend } from '../../../../utils/appendFieldUtils'
 
 const ERROR_MSG = 'Enter details of the review'
-const UPDATE_ERROR_MSG = 'Enter an update on the review'
-const TOO_LONG_ERROR_MSG = (isUpdate: boolean | undefined, maxLengthChars: number) =>
-  `${isUpdate ? 'Update to the details' : 'Details of the review'} must be ${maxLengthChars.toLocaleString()} characters or less`
+const TOO_LONG_ERROR_MSG = 'Details of the review must be 4,000 characters or less'
 
-export const schemaFactory = async (req: Request, res: Response) => {
-  const maxLengthChars = req.journeyData.isUpdate
-    ? getMaxCharsAndThresholdForAppend(res.locals.user.displayName, req.journeyData!.review!.summary).maxLengthChars
-    : 4000
+export const schema = createSchema({
+  summary: z
+    .string({ message: ERROR_MSG })
+    .max(4000, TOO_LONG_ERROR_MSG)
+    .refine(val => val && val.trim().length > 0, ERROR_MSG),
+})
 
-  return createSchema({
-    summary: z
-      .string({ message: req.journeyData.isUpdate ? UPDATE_ERROR_MSG : ERROR_MSG })
-      .max(maxLengthChars, TOO_LONG_ERROR_MSG(req.journeyData.isUpdate, maxLengthChars))
-      .refine(val => val && val.trim().length > 0, req.journeyData.isUpdate ? UPDATE_ERROR_MSG : ERROR_MSG),
-  })
-}
-
-export type SchemaType = z.infer<Awaited<ReturnType<typeof schemaFactory>>>
+export type SchemaType = z.infer<typeof schema>
