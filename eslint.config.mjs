777e95b20947c0da356c837504312df175b7f18d
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2025-01-29 13:05:30 +0000

    fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,12 @@ import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 export default [
   ...hmppsConfig({
     extraIgnorePaths: ['assets'],
+    extraPathsAllowingDevDependencies: ['**/*.cy.ts'],
   }),
   {
     rules: {
       'dot-notation': 'off',
       'import/prefer-default-export': 0,
-      'import/no-extraneous-dependencies': ["error", {"devDependencies": ["**/*.cy.ts"]}]
     },
   },
 ]
