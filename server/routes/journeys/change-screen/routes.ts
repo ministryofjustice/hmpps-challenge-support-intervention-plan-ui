478dbab3648ded2046a8d1591932e53c5bc1a720
Jack Reeve <jack.reeve@digital.justice.gov.uk>
2025-02-21 10:20:04 +0000

    removed journey state guard

--- server/routes/journeys/change-screen/routes.ts
+++ server/routes/journeys/change-screen/routes.ts
@@ -1,6 +1,4 @@
-import { Request } from 'express'
 import { ChangeScreenCheckAnswersRoutes } from './check-answers/routes'
-import journeyStateGuard, { isMissingValues } from '../../../middleware/journeyStateGuard'
 import { Services } from '../../../services'
 import { JourneyRouter } from '../base/routes'
 import StartJourneyRoutes from './start/routes'
@@ -26,13 +24,7 @@ export const ChangeScreenRoutes = ({ services, path }: { services: Services; pat
 
   router.use('/csip-record/:csipRecordId/change-screen/start', StartJourneyRoutes(services))
   router.use('/check-change-screen', CheckRoutes())
-  router.use(path, journeyStateGuard(guard, services.appInsightsClient))
   router.use(path, Routes(services))
 
   return router
-}
-
-const guard = {
-  'check-answers': (req: Request) =>
-    isMissingValues(req.journeyData.saferCustodyScreening!, ['outcomeType', 'reasonForDecision']) ? '' : undefined,
 }
