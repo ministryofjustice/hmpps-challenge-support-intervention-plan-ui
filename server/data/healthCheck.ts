08666f83c081c31b4c51058f9fff0677713059b8
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-09-16 10:50:45 +0100

    updated dependencies

--- server/data/healthCheck.ts
+++ server/data/healthCheck.ts
@@ -17,27 +17,38 @@ export function serviceCheckFactory(
   agentOptions: AgentConfig,
   serviceTimeout: ServiceTimeout = new ServiceTimeout(),
 ): ServiceCheck {
-  const keepaliveAgent = url.startsWith('https') ? new HttpsAgent(agentOptions) : new Agent(agentOptions)
+  return async () => {
+    const keepaliveAgent = url.startsWith('https') ? new HttpsAgent(agentOptions) : new Agent(agentOptions)
+    try {
+      let request = superagent.get(url)
 
-  return () =>
-    new Promise((resolve, reject) => {
-      superagent
-        .get(url)
-        .agent(keepaliveAgent)
+      if (!process.env['JEST_WORKER_ID']) {
+        request = request.agent(keepaliveAgent)
+      }
+
+      request = request
         .retry(2, err => {
           if (err) logger.info(`Retry handler found API error with ${err.code} ${err.message} when calling ${name}`)
-          return undefined // retry handler only for logging retries, not to influence retry logic
+          return undefined
         })
         .timeout(serviceTimeout)
-        .end((error, result) => {
-          if (error) {
-            logger.error(error.stack, `Error calling ${name}`)
-            reject(error)
-          } else if (result.status === 200) {
-            resolve('UP')
-          } else {
-            reject(result.status)
-          }
-        })
-    })
+
+      if (process.env['JEST_WORKER_ID']) {
+        request = request.set('Connection', 'close')
+      }
+
+      const result = await request
+      if (result.status === 200) {
+        return 'UP'
+      }
+      throw result.status
+    } catch (error) {
+      if (error instanceof Error) {
+        logger.error(error.stack, `Error calling ${name}`)
+      }
+      throw error
+    } finally {
+      keepaliveAgent.destroy()
+    }
+  }
 }
