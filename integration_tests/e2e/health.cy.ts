3df4efd99c59c32b512c6362368ce8beb45ea89f
Paul Massey <paul.massey@digital.justice.gov.uk>
2025-09-18 11:02:33 +0100

    Improve health check e2e tests
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- integration_tests/e2e/health.cy.ts
+++ integration_tests/e2e/health.cy.ts
@@ -13,12 +13,14 @@ context('Healthcheck', () => {
     })
 
     it('Health check page is visible and services report as UP', () => {
-      cy.request({ url: '/health', method: 'GET', failOnStatusCode: false }).then(response => {
+      cy.request({ url: '/health', method: 'GET' }).then(response => {
         expect(response.body.status).to.equal('UP')
         expect(response.body.components.hmppsAuth.status).to.equal('UP')
         expect(response.body.components.tokenVerification.status).to.equal('UP')
         expect(response.body.components.componentApi.status).to.equal('UP')
         expect(response.body.components.csipApi.status).to.equal('UP')
+        expect(response.body.components.prisonerSearchApi.status).to.equal('UP')
+        expect(response.body.components.prisonApi.status).to.equal('UP')
       })
     })
 
@@ -36,18 +38,31 @@ context('Healthcheck', () => {
       cy.task('reset')
       cy.task('stubAuthPing')
       cy.task('stubTokenVerificationPing', 500)
+      cy.task('stubPrisonApiPing')
+      cy.task('stubPrisonerSearchApiPing')
+      cy.task('stubComponentsPing')
+      cy.task('stubCsipApiPing')
     })
 
     it('Reports correctly when token verification down', () => {
       cy.request({ url: '/health', method: 'GET', failOnStatusCode: false }).then(response => {
+        expect(response.body.status).to.equal('DOWN')
         expect(response.body.components.hmppsAuth.status).to.equal('UP')
         expect(response.body.components.tokenVerification.status).to.equal('DOWN')
         expect(response.body.components.tokenVerification.details).to.contain({ status: 500 })
+        expect(response.body.components.componentApi.status).to.equal('UP')
+        expect(response.body.components.csipApi.status).to.equal('UP')
+        expect(response.body.components.prisonerSearchApi.status).to.equal('UP')
+        expect(response.body.components.prisonApi.status).to.equal('UP')
       })
     })
+    
+    it('Ping is visible and UP', () => {
+      cy.request('/ping').its('body.status').should('equal', 'UP')
+    })
 
-    it('Health check page is visible and DOWN', () => {
-      cy.request({ url: '/health', method: 'GET', failOnStatusCode: false }).its('body.status').should('equal', 'DOWN')
+    it('Info is visible', () => {
+      cy.request('/info').its('body').should('exist')
     })
   })
 })
