fc82717532ae1368fc1099b45ebe046d7b8f1cbc
Paul Massey <paul.massey@digital.justice.gov.uk>
2025-12-04 13:04:37 +0000

    Updated scripts
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,17 +2,19 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
+    // Needed by esbuild for watching files during development
+    "node_modules/@parcel/watcher@2.5.1": "ALLOW",
     // Needed by Sentry CLI for error reporting and source map uploads
     'node_modules/@sentry/cli@2.58.2': 'ALLOW',
     // Needed by Cypress for running tests
-    'node_modules/cypress@15.7.0': 'ALLOW',
+    'node_modules/cypress@15.7.1': 'ALLOW',
     // Provides native integration, supporting performance monitoring and bunyan logging
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // Needed by esbuild for watching files during development
-    'node_modules/esbuild@0.27.0': 'ALLOW',
+    'node_modules/esbuild@0.27.1': 'ALLOW',
     // Needed by Jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by Jest and ESLint
-    'node_modules/unrs-resolver@1.9.2': 'ALLOW',
+    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
 })
