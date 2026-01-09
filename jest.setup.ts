91fc56f8b27aba4a684ae2fb940793403d0bc2a9
Paul Massey <paul.massey@digital.justice.gov.uk>
2026-01-09 15:47:23 +0000

    Remove Unused '@ts-expect-error' directive.
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom'
 
-// @ts-expect-error setImmediate args have any types
 globalThis.setImmediate = globalThis.setImmediate || ((fn, ...args) => global.setTimeout(fn, 0, ...args))
 
 // Reset JSDOM
