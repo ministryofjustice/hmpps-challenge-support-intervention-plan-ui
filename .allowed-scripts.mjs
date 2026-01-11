a9aad10f13b6e6ad467b86e0d86667c5d5d181bf
Paul Massey <theDustRoom@users.noreply.github.com>
2026-01-11 16:20:51 +0000

    LSM-268 - Fix Vulnerabilities (#574)
    * Upgrade dependencies
    
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>
    
    * Remove Unused '@ts-expect-error' directive.
    
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>
    
    * Rollback moj-frontend to 7.1.1
    
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>
    
    ---------
    
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -5,13 +5,13 @@ export default configureAllowedScripts({
     // Needed by esbuild for watching files during development
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
     // Needed by Sentry CLI for error reporting and source map uploads
-    'node_modules/@sentry/cli@2.58.3': 'ALLOW',
+    'node_modules/@sentry/cli@3.0.3': 'ALLOW',
     // Needed by Cypress for running tests
-    'node_modules/cypress@15.7.1': 'ALLOW',
+    'node_modules/cypress@15.8.2': 'ALLOW',
     // Provides native integration, supporting performance monitoring and bunyan logging
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // Needed by esbuild for watching files during development
-    'node_modules/esbuild@0.27.1': 'ALLOW',
+    'node_modules/esbuild@0.27.2': 'ALLOW',
     // Needed by Jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by Jest and ESLint
