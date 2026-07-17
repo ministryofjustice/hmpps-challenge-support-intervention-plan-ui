b90e4dd4362efa136fdcc9f990b80e8fee5e1d2d
Mohammed Debashi <mdebashi@gmail.com>
2026-07-17 16:06:56 +0100

    Feature/jda 403 create feature flag for frontend jda work (#623)
    * Add reusable Suggested Case Notes widget partial
    
    * JDA-403 - create feature flag for frontend jda work
    
    * Remove suggested case notes partial files from JDA-403 branch
    
    * feature toggle for env, feature and prisons
    
    * Remove unrelated stylesheet from JDA-403 branch
    
    ---------
    
    Co-authored-by: Mohammed Debashi <mohammed.debashi@justice.gov.uk>

--- server/config.ts
+++ server/config.ts
@@ -13,6 +13,18 @@ function get<T>(name: string, fallback: T, options = { requireInProduction: fals
 
 const requiredInProduction = { requireInProduction: true }
 
+function getCsv(name: string, fallback: string[] = []): string[] {
+  const envVar = process.env[name]
+  if (!envVar) {
+    return fallback
+  }
+
+  return envVar
+    .split(',')
+    .map(it => it.trim())
+    .filter(Boolean)
+}
+
 export class AgentConfig {
   // Sets the working socket to timeout after timeout milliseconds of inactivity on the working socket.
   timeout: number
@@ -144,5 +156,7 @@ export default {
   },
   features: {
     changeFlows: get('FEATURE_CHANGE_FLOWS', 'false') === 'true',
+    csipAssistEnabled: get('FEATURE_CSIP_ASSIST', 'false') === 'true',
+    csipAssistActivePrisons: getCsv('CSIP_ASSIST_ACTIVE_PRISONS').map(it => it.toUpperCase()),
   },
 }
