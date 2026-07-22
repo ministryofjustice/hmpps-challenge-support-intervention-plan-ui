dc4d7a191b853eaee1beabc0c2c51b741849c47c
Mohammed Debashi <mdebashi@gmail.com>
2026-07-22 15:12:33 +0100

    Feature/suggested case notes widget only (#625)
    * Add reusable Suggested Case Notes widget partial
    
    * Sync CSIP assist feature-flag setup from JDA-403 branch
    
    * Refine suggested case notes widget layout and styling
    
    * Gate suggested case notes widget behind CSIP assist feature toggle
    
    * import error
    
    ---------
    
    Co-authored-by: Mohammed Debashi <mohammed.debashi@justice.gov.uk>

--- server/routes/journeys/record-investigation/usual-behaviour-presentation/controller.ts
+++ server/routes/journeys/record-investigation/usual-behaviour-presentation/controller.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from 'express'
 import { SchemaType } from './schemas'
+import csipAssistEnabled from '../../../../utils/featureToggles'
 
 export class UsualBehaviourPresentationController {
   GET = async (req: Request, res: Response) => {
     const personsUsualBehaviour =
       res.locals.formResponses?.['personsUsualBehaviour'] ?? req.journeyData.investigation?.personsUsualBehaviour
+    const showSuggestedCaseNotesWidget = csipAssistEnabled(
+      res.locals.user.activeCaseLoad?.caseLoadId || res.locals.user.activeCaseLoadId,
+    )
+
     res.render('record-investigation/usual-behaviour-presentation/view', {
       personsUsualBehaviour,
       backUrl: '../record-investigation',
       maxLengthChars: 4000,
+      showSuggestedCaseNotesWidget,
     })
   }
 
